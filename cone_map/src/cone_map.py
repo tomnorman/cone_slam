17599f1c991215641a9e144ad97aa8a159f8809b
tom.norman <tom.norman@campus.technion.ac.il>
2019-05-16 10:51:20 +0300

    start debug cone_map

--- cone_map/src/cone_map.py
+++ cone_map/src/cone_map.py
@@ -5,55 +5,112 @@ from sklearn.cluster import DBSCAN
 import numpy as np
 
 def callBack(msg):
-	np_arr = create_np_arr(msg)
-	#first row is position so ignore
-	#second row is amount of cones
-	pose = np_arr[0]
-	NRED = np_arr[1,0];
-	NBLUE = np_arr[1,1];
-	non_cones = 2
-	if np_arr.shape[0] <= non_cones:
-		return
-	red_cones = np_arr[non_cones:non_cones+NRED]
-	blue_cones = np_arr[non_cones+NRED:]
-	if NRED:
-		red_labels = DBSCAN(eps = 0.5, min_samples = 2).fit_predict(red_cones)
+    # DBSCAN consts
+    eps = 0.5
+    min_samples = 2
+    # Float32MultiArray consts
+    cols = 4 #3D == 4, 2D == 3
+    yellow = 0 #number to identify cone
+    blue = 1 #number to identify cone
 
-	if NBLUE:
-		blue_labels = DBSCAN(eps = 0.5, min_samples = 2).fit_predict(blue_cones)
+    np_arr = create_np_arr(msg)
+    #first row is position so ignore
+    #second row is amount of cones so ignore
+    pose = np_arr[0]
+    NYELLOW = np_arr[1,0];
+    NBLUE = np_arr[1,1];
+    none_cones = 2 #constant
+    yellow_cones = np.array([])
+    blue_cones = np.array([])
+    if np_arr.shape[0] > none_cones:
+        if NYELLOW:
+            yellow_ORBs = np_arr[none_cones:none_cones+NYELLOW]
+            yellow_cones = create_centers(yellow_ORBs, eps, min_samples)
+        if NBLUE:
+            blue_ORBs = np_arr[none_cones+NYELLOW:]
+            blue_cones = create_centers(blue_ORBs, eps, min_samples)
 
-	# arr = create_Point_arr(points)
-	# data.points = arr
-	# pub.publish(data)
+    FMA = create_Float32MultiArray(pose, yellow_cones, yellow, blue_cones, blue, none_cones, cols)
+    pub.publish(FMA)
 
-def create_Float32MultiArray(points):
-	arr = []
-	# something
-	return arr
+def create_centers(samples, eps, min_samples):
+    clusters = DBSCAN(eps = eps, min_samples = min_samples).fit_pyellowict(samples)
+    sorted_clusters_idxs = np.argsort(clusters)
+    clusters = clusters[sorted_clusters_idxs]
+    samples = samples[sorted_clusters_idxs] #clusteyellow smaples
+    '''
+    cone -1 #-1 is ouliers
+    cone -1 #-1 is ouliers
+    ...
+    cone 0
+    cone 0
+    ...
+    cone i
+    cone i
+    cone i
+    ...
+    cone n
+    cone n
+    '''
+    centers = []
+    for i in range(clusters[-1]+1): #clusters[-1] == np.max(clusters)
+        centers += [np.mean(samples[clusters == i],axis = 0)] #get the mean of the cluster
+        #TODO: geometric mean??
+    return np.array(centers)
+
+
+def create_Float32MultiArray(pose, yellow_cones, yellow, blue_cones, blue, none_cones, cols):
+    rows = none_cones+yellow_cones.shape[0]+blue_cones.shape[0]
+    FMA = Float32MultiArray()
+    FMA.layout.dim.append(MultiArrayDimension())
+    FMA.layout.dim[0].label = "rows"
+    FMA.layout.dim[0].size = rows
+    FMA.layout.dim[0].stride = cols*rows;
+    FMA.layout.dim.append(MultiArrayDimension())
+    FMA.layout.dim[1].label = "cols"
+    FMA.layout.dim[1].size = cols
+    FMA.layout.dim[1].stride = cols
+    FMA.layout.data_offset = 0
+    FMA.data = []
+    #pose
+    for i in range(pose.shape[0]):
+        FMA.data += [pose[i]]
+    #yellow cones
+    for i in range(yellow_cones.shape[0]):
+        for j in range(cols-1): #3D or 2D
+            FMA.data += [yellow_cones[i][j]]
+        FMA.data += [yellow]
+    #blue cones
+    for i in range(blue_cones.shape[0]):
+        for j in range(cols-1): #3D or 2D
+            FMA.data += [blue_cones[i][j]]
+        FMA.data += [blue]
+
+    return FMA
 
 def create_np_arr(msg):
-	data = msg.data
-	layout = msg.layout
-	dim0 = layout.dim[0]
-	dim1 = layout.dim[1]
-	rows = dim0.size
-	cols = dim1.size
-	arr = []
-	for i in range(rows):
-		row = []
-		for j in range(cols):
-			row += [data[i*cols+j]]
-		arr += [row]
-	return np.array(arr)
+    data = msg.data
+    layout = msg.layout
+    dim0 = layout.dim[0]
+    dim1 = layout.dim[1]
+    rows = dim0.size
+    cols = dim1.size
+    arr = []
+    for i in range(rows):
+        row = []
+        for j in range(cols):
+            row += [data[i*cols+j]]
+        arr += [row]
+    return np.array(arr)
 
 def listener():
-	global pub
-	rospy.init_node('listener', anonymous = True)
-	topic_in = rospy.get_param('/cone_slam/points_topic', 'points_map')
-	topic_out = rospy.get_param('/cone_slam/cone_topic', 'cone_map')
-	pub = rospy.Publisher(topic_out, Float32MultiArray, queue_size = 10)
-	rospy.Subscriber(topic_in, Float32MultiArray, callBack)
-	rospy.spin()
+    global pub
+    rospy.init_node('listener', anonymous = True)
+    topic_in = rospy.get_param('/cone_slam/points_topic', 'points_map')
+    topic_out = rospy.get_param('/cone_slam/cone_topic', 'cone_map')
+    pub = rospy.Publisher(topic_out, Float32MultiArray, queue_size = 10)
+    rospy.Subscriber(topic_in, Float32MultiArray, callBack)
+    rospy.spin()
 
 if __name__ == '__main__':
-	listener()
+    listener()
--- orb_slam2/src/MapPublisher.cc
+++ orb_slam2/src/MapPublisher.cc
@@ -10,18 +10,19 @@ MapPublisher::MapPublisher(Map* pMap) : mpMap(pMap) {
 
 void MapPublisher::PublishPoints(cv::Mat mOw)
 {
-	REDpoints.clear();
+	YELLOWpoints.clear();
 	BLUEpoints.clear();
 	std_msgs::Float32MultiArray m;
 	MakeConeMap();
-	unsigned int NRED = REDpoints.size();
+	unsigned int NYELLOW = YELLOWpoints.size();
 	unsigned int NBLUE = BLUEpoints.size();
-	// cout << NRED << " RED cones, " << NBLUE << " blue cones.\n";
+	// cout << NYELLOW << " YELLOW cones, " << NBLUE << " blue cones.\n";
 
+	// Consts
 	/*
 	create array msg:
 		mOw,0
-		NRED,NBLUE,0,0
+		NYELLOW,NBLUE,0,0
 		x1,y1,z1,color_enum
 		...
 		xi,yi,zi,color_enum
@@ -30,7 +31,7 @@ void MapPublisher::PublishPoints(cv::Mat mOw)
 
 	int mOwRows = mOw.rows; // should be 3
 	int cols = 4;
-	int rows = 2+NRED+NBLUE; // first row-mOw, second row-amount of cones, all else is points
+	int rows = 2+NYELLOW+NBLUE; // first row-mOw, second row-amount of cones, all else is points
 	m.layout.dim.push_back(std_msgs::MultiArrayDimension());
 	m.layout.dim[0].label = "rows";
 	m.layout.dim[0].size = rows;
@@ -39,6 +40,7 @@ void MapPublisher::PublishPoints(cv::Mat mOw)
 	m.layout.dim[1].label = "cols";
 	m.layout.dim[1].size = cols; // for i>0: data[i,3] = cone type
 	m.layout.dim[1].stride = cols;
+	m.layout.data_offset = 0;
 
 	// mOw [3x1]
 	for (int i = 0; i < mOwRows; ++i)
@@ -46,15 +48,15 @@ void MapPublisher::PublishPoints(cv::Mat mOw)
 	m.data.push_back(static_cast<float>(0));
 
 	// amount of points
-	m.data.push_back(static_cast<float>(NRED));
+	m.data.push_back(static_cast<float>(NYELLOW));
 	m.data.push_back(static_cast<float>(NBLUE));
 	m.data.push_back(static_cast<float>(0));
 	m.data.push_back(static_cast<float>(0));
 
-	// REDpoints
-	for (int i = 0; i < NRED; ++i)
+	// YELLOWpoints
+	for (int i = 0; i < NYELLOW; ++i)
 		for (int j = 0; j < cols; ++j)
-			m.data.push_back(REDpoints[i][j]); // x,y,z
+			m.data.push_back(YELLOWpoints[i][j]); // x,y,z
 
 	// BLUEpoints
 	for (int i = 0; i < NBLUE; ++i)
@@ -66,14 +68,15 @@ void MapPublisher::PublishPoints(cv::Mat mOw)
 void MapPublisher::MakeConeMap()
 {
 	vector<vector<float>> points = mpMap->GetAllConePoints();
+	//returns 4D vector
 	// cout << "there are " << points.size() << " points\n";
 
-	// for all cone -> get vectors of RED and BLUE
+	// for all cone -> get vectors of YELLOW and BLUE
 	for(auto &i : points)
 	{
 		// cout <<static_cast<int>(i.back()) << " point type\n";
-		if (static_cast<int>(i.back()) == 0) // RED
-			REDpoints.push_back(i);
+		if (static_cast<int>(i.back()) == 0) // YELLOW
+			YELLOWpoints.push_back(i);
 		else if(static_cast<int>(i.back()) == 1) // BLUE
 			BLUEpoints.push_back(i);
 	}
