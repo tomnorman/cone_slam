fcb2b7d1cb02ccbf61c7b6959977eb7271da954c
tom.norman <tom.norman@campus.technion.ac.il>
2019-08-08 23:27:04 +0300

    rviz works, needs to add the config file and show the path

--- debug_unit/src/rviz.py
+++ debug_unit/src/rviz.py
@@ -9,14 +9,17 @@ from geometry_msgs.msg import Vector3, TransformStamped, Point
 from visualization_msgs.msg import Marker, MarkerArray
 from custom_msgs.msg import slam_test_msg
 
+i = 0
+formula_path = []
+
 def publish_to_rviz(msg):
     br = tf2_ros.TransformBroadcaster()
 
-    zeros = [0 for i in range(len(msg.yellow_cluster_y))]
+    zeros = [0 for j in range(len(msg.yellow_cluster_y))]
     # yellow_cluster
-    yellow_cluster = create_cloud(msg.yellow_cluster_x, msg.yellow_cluster_y,zeros,ColorRGBA(1,0,0,0))
+    yellow_cluster = create_cloud(msg.yellow_cluster_x, msg.yellow_cluster_y,zeros,ColorRGBA(1,1,0,1))
     # blue_cluster
-    blue_cluster= create_cloud(msg.blue_cluster_x, msg.blue_cluster_y,zeros,ColorRGBA(0,0,1,0))
+    blue_cluster= create_cloud(msg.blue_cluster_x, msg.blue_cluster_y,zeros,ColorRGBA(0,0,1,1))
     # formula tf
     formula_tf = TransformStamped()
     formula_tf.header.stamp = rospy.Time.now()
@@ -30,26 +33,30 @@ def publish_to_rviz(msg):
     formula_tf.transform.rotation.z = 0
     formula_tf.transform.rotation.w = 1
 
-    markers_pub.publish(yellow_cluster)
-    markers_pub.publish(blue_cluster)
+    marker_array = MarkerArray([yellow_cluster, blue_cluster])
+    markers_pub.publish(marker_array)
     br.sendTransform(formula_tf)
 
 
 def create_cloud(xs, ys, zs, color):
+    global i
     header = Header()
     header.stamp = rospy.Time.now()
     header.frame_id = 'world'
 
     marker = Marker()
     marker.header = header
-    marker.ns = 'cluster'
+    marker.id = i
+    i += 1
+    i %= 100 #to bound i
+    print(i)
     marker.type = marker.POINTS
-    marker.scale = Vector3(100,100,100)
-    marker.color = color
-    marker.lifetime = rospy.Duration.from_sec(100.0) #one sec
-    #marker.frame_locked
+    marker.scale = Vector3(0.1,0.1,0.1)
+    marker.lifetime = rospy.Duration.from_sec(1.0)
     points = [Point(x,y,z) for (x,y,z) in zip(xs,ys,zs)]
     marker.points = points
+    colors = [color for j in range(len(points))]
+    marker.colors = colors
 
     return marker
 
@@ -57,6 +64,6 @@ if __name__ == '__main__':
     rospy.init_node('debug_unit')
     topic_in = rospy.get_param('/cones_map/test_topic', 'slam_test')
     rospy.Subscriber(topic_in, slam_test_msg , publish_to_rviz)
-    markers_pub = rospy.Publisher("visualization_marker", Marker, queue_size = 100)
+    markers_pub = rospy.Publisher("visualization_marker_array", MarkerArray, queue_size = 100)
     rospy.spin()
    
