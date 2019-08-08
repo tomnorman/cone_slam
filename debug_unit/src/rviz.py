44d8e551d5fb8fe928155650bf6431d8dedefcd7
tom.norman <tom.norman@campus.technion.ac.il>
2019-08-08 22:15:38 +0300

    doesnt show markers

--- debug_unit/src/rviz.py
+++ debug_unit/src/rviz.py
@@ -5,21 +5,23 @@ import tf_conversions
 
 import tf2_ros
 from std_msgs.msg import ColorRGBA, Header
-from geometry_msgs.msg import TransformStamped, Point
+from geometry_msgs.msg import Vector3, TransformStamped, Point
 from visualization_msgs.msg import Marker, MarkerArray
 from custom_msgs.msg import slam_test_msg
 
 def publish_to_rviz(msg):
     br = tf2_ros.TransformBroadcaster()
 
+    zeros = [0 for i in range(len(msg.yellow_cluster_y))]
     # yellow_cluster
-    yellow_cluster = create_cloud(msg.yellow_cluster_x, msg.yellow_cluster_y, np.zeros(len(msg.yellow_cluster_y)),ColorRGBA())
+    yellow_cluster = create_cloud(msg.yellow_cluster_x, msg.yellow_cluster_y,zeros,ColorRGBA(1,0,0,0))
     # blue_cluster
-    blue_cluster= create_cloud(msg.blue_cluster_x, msg.blue_cluster_y, np.zeros(len(msg.blue_cluster_y)))
+    blue_cluster= create_cloud(msg.blue_cluster_x, msg.blue_cluster_y,zeros,ColorRGBA(0,0,1,0))
     # formula tf
     formula_tf = TransformStamped()
     formula_tf.header.stamp = rospy.Time.now()
     formula_tf.header.frame_id = 'world'
+    formula_tf.child_frame_id = 'formula'
     formula_tf.transform.translation.x = msg.pos_x
     formula_tf.transform.translation.y = msg.pos_y
     formula_tf.transform.translation.z = 0;
@@ -34,25 +36,27 @@ def publish_to_rviz(msg):
 
 
 def create_cloud(xs, ys, zs, color):
-    header = Marker.Header()
+    header = Header()
     header.stamp = rospy.Time.now()
     header.frame_id = 'world'
 
-    points = [Point(x,y,z) for (x,y,z) in (xs,ys,zs)]
     marker = Marker()
     marker.header = header
-    marker.type = 8
+    marker.ns = 'cluster'
+    marker.type = marker.POINTS
+    marker.scale = Vector3(100,100,100)
     marker.color = color
-    marker.lifetime = 1
+    marker.lifetime = rospy.Duration.from_sec(100.0) #one sec
     #marker.frame_locked
+    points = [Point(x,y,z) for (x,y,z) in zip(xs,ys,zs)]
     marker.points = points
 
-    return points
+    return marker
 
 if __name__ == '__main__':
     rospy.init_node('debug_unit')
     topic_in = rospy.get_param('/cones_map/test_topic', 'slam_test')
     rospy.Subscriber(topic_in, slam_test_msg , publish_to_rviz)
-    markers_pub = rospy.Publisher("visualization_marker", MarkerArray, queue_size = 1)
+    markers_pub = rospy.Publisher("visualization_marker", Marker, queue_size = 100)
     rospy.spin()
    
